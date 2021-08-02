10d079cfc82167847a265929e5a0d710dbec6e50
Masga Satria Wirawan <45129872+mxsgx@users.noreply.github.com>
2021-08-02 21:11:33 +0700

    Update handler for event "chat:message"

--- controllers/ws/chat/message.js
+++ controllers/ws/chat/message.js
@@ -1,9 +1,6 @@
 module.exports = function (socket, fastify) {
   return function (data) {
-    console.log('Payload: ', data);
-
-    socket.emit('chat:receive', {
-      text: 'Hi! from server-side',
-    });
+    socket.emit('chat:receive', data);
+    socket.broadcast.emit('chat:receive', data);
   };
 };
